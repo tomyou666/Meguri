d83ab89b33785beae354fa050e02014a91dc2de6
tomyou666 <tomyou625@yahoo.co.jp>
2026-07-04 13:27:17 +0900

    chore: add react-scan dependency

--- front/frontend/src/main.tsx
+++ front/frontend/src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { scan } from 'react-scan';
 import './index.css';
 import { installExternalLinkDelegation } from '@/lib/externalLinkDelegation';
 import App from './App';
+
+if (import.meta.env.DEV) {
+	scan({ enabled: true });
+}
 
 installExternalLinkDelegation();
 
